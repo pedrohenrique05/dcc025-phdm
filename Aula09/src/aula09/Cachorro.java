c646fa2b458e84a2e1f2073f3f039a71c14e9adc
pedro <pedro@172.18.133.64>
2018-09-15 15:41:53 -0300

    criando classe filho de animal

---
+++ Aula09/src/aula09/Cachorro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula09;
+import java.util.Scanner;
+/**
+ *
+ * @author pedro
+ */
+public class Cachorro extends Animal {
+    
+
+    
+    @Override
+    /**
+     * 
+     */
+    public void emiteSom(){
+        super.emiteSom();
+        System.out.println("Cachorro late!");
+    }
+    
+}
