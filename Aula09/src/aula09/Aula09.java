01b5ad31750a9bd66e86300cb80ed6d2dc167703
pedro <pedro@172.18.133.64>
2018-09-15 15:41:27 -0300

    Aula 09 - herança... class <nome da classe> extends <nome classe pai>

---
+++ Aula09/src/aula09/Aula09.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula09;
+
+/**
+ *
+ * @author pedro
+ */
+public class Aula09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Animal animal = new Animal();
+        animal.come();
+        animal.dorme();
+        animal.emiteSom();
+        
+        System.out.println("cachorro a baixo");
+        Cachorro cachorro = new Cachorro();
+        
+        cachorro.come();
+        cachorro.setNome("Fred");
+        cachorro.getNome();
+        cachorro.emiteSom();
+        
+    }
+    
+}
