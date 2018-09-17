eb7727792c9a20d090ad76eb035e5842f133bea3
pedro <pedro@172.18.133.64>
2018-09-17 16:24:11 -0300

    Adicionando atributos a classe pai de gato.(e filho de animal)

--- Aula09/src/aula09/Felino.java
+++ Aula09/src/aula09/Felino.java
@@ -11,5 +11,23 @@ package aula09;
  * @see filho de Animal e pai de Gato
  */
 public class Felino extends Animal{
+    private String subEspecie;
     
+    @Override
+     /**
+     * @see chamando o metodo setEspecie da classe pai Animal
+     */
+    public void setEspecie(String subEspecie){
+        this.subEspecie = subEspecie;
+        super.setEspecie("Felidae");
+    }
+    
+    @Override
+        /**
+     * @see chamando o metodo getEspecie da classe pai Animal
+     */
+    public void getEspecie(){
+        super.getEspecie();
+        System.out.println("SubEspecie: " + this.subEspecie);
+    }
 }
