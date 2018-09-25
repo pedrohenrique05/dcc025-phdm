dfdfe5e142b09041e494d005ae0a798102ca29dd
pedro <pedro@172.18.133.64>
2018-09-24 21:08:46 -0300

    Criando classe concreto filho

---
+++ Aula10/src/aula10/Dragao.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula10;
+
+/**
+ *
+ * @author pedro
+ */
+public class Dragao extends Pokemon{
+    protected  String nome;
+    Dragao(String nome){
+        super.setNome(nome);
+        this.nome = nome;
+    }
+    /**
+     *
+     */
+    @Override
+    public void emiteSom(){
+        System.out.println("muha");
+    }
+        @Override
+    public boolean equals(Object obj){
+        return false;
+    }
+    
+    @Override
+    public String toString(){
+        return getNome();
+    }
+    public void setNome(String nome){
+        
+    }
+    @Override
+    public void setNivel(int nivel){
+        
+    }
+    @Override
+    public void setHp(int hp){
+        
+    }
+    @Override
+    public void setAtaque(int ataque){
+        
+    }
+    @Override
+    public void setDefesa(int defesa){
+        
+    }
+    
+}
