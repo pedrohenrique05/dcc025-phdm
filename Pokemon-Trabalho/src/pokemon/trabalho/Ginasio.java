5b13fdbe115c91395ff50b2eb7e3edd3c8a61a28
pedro <pedro@172.18.133.64>
2018-09-30 20:28:12 -0300

    ginasio

---
+++ Pokemon-Trabalho/src/pokemon/trabalho/Ginasio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon.trabalho;
+
+/**
+ *
+ * @author pedro
+ */
+public class Ginasio extends Pokedex{
+    private String nomeGinasio, MestreGinasio;
+    private String [] pokemons = new String[10];
+    
+    
+}
