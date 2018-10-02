6d1b6f10f9e618abaf831e7d1c7e8bd9d331e461
pedro <pedro@172.18.133.64>
2018-10-02 16:18:30 -0300

    criando metodos da pokedex

--- Pokemon-Trabalho/src/pokemon/trabalho/Pokedex.java
+++ Pokemon-Trabalho/src/pokemon/trabalho/Pokedex.java
@@ -10,5 +10,34 @@ package pokemon.trabalho;
  * @author pedro
  */
 public class Pokedex extends Jogador {
-    private static int numPokemons = 0, ginasio = 0;
+    private static  int numPokemons = 0;
+    private static  int numGinasio = 0;
+    
+    /**
+     * adicionando o numero de pokemons na pokedex.
+     */
+    public void setNumPokemon(){
+        this.numPokemons++;
+    }
+    
+    /**
+     * adicionando o numero de ginasio que está na pokedex.
+     */
+    public void setNumGinasio(){
+        this.numGinasio++;
+    }
+    /**
+     * 
+     * @return o numero de pokemons que possui na pokedex.
+     */
+    public int getNumPokemon(){
+        return this.numPokemons;
+    }
+    /**
+     * 
+     * @return o numero de ginasio que possui na pokedex.
+     */
+    public int getNumGinasio(){
+        return this.numGinasio;
+    }
 }
