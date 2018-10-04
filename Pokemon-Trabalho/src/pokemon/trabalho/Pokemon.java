760900359bad6727fbf4df33120ca4b8f71b9813
pedro <pedro@172.18.133.64>
2018-10-04 16:03:08 -0300

    criando os metodos set e get

--- Pokemon-Trabalho/src/pokemon/trabalho/Pokemon.java
+++ Pokemon-Trabalho/src/pokemon/trabalho/Pokemon.java
@@ -12,13 +12,17 @@ package pokemon.trabalho;
 public  class Pokemon extends Pokedex{
     
     private String nomePokemon, sexoPokemon, habilidade, fraqueza, descricao;
-    private int altura, peso, numId = 0, numIdEvolucao = 0;
-    private boolean evolucao = false;
+    private int altura, peso, numIdEvolucao = 0, evolucao = 0;
+    private static int numId = 0;
     
-    @Override
+    
+    /**
+     *classe que tem por finalidade chamar a classe pai 
+     */
+    /*@Override
     public void setPokemon(){
         super.setPokemon();;
-    }
+    }*/
     /** 
      * setando o nome do pokemon
      * @param nomePokemon
@@ -26,50 +30,117 @@ public  class Pokemon extends Pokedex{
     public void setNomePokemon(String nomePokemon){
         this.nomePokemon = nomePokemon;
     }
-    
+    /**
+     * setando o sexo do pokemon
+     * @param sexoPokemon 
+     */
     public void setSexoPokemon(String sexoPokemon){
         this.sexoPokemon = sexoPokemon;
     }
-    
+    /**
+     * setando a habilidade do pokemon
+     * @param habilidade 
+     */
     public void setHabilidade(String habilidade){
         this.habilidade = habilidade;
     }
-    
+    /**
+     * setando a fraqueza do pokemon
+     * @param fraqueza 
+     */
     public void setFraqueza(String fraqueza){
         this.fraqueza = fraqueza;
     }
-    
+    /**
+     * setando a descrição que o pokemon possui
+     * @param descricao 
+     */
     public void setDescricao(String descricao){
         this.descricao = descricao;
     }
-    
+    /**
+     * setando a altura que o pokemon possui
+     * @param altura 
+     */
     public void setAltura(int altura){
         this.altura = altura;
     }
-    
+    /**
+     * setando o peso que o pokemon possui
+     * @param peso 
+     */
     public void setPeso(int peso){
         this.peso = peso;
     }
+    /**
+     * setando o id de identificação do pokemon
+     */
     public void setNumId(){
-        this.numId++;
+        Pokemon.numId++;
     }
-    
+    /**
+     * setando o id do pokemon que esse pokemon evoluiu. 
+     * @param numIdEvolucao 
+     */
     public void setNumIdEvolucao(int numIdEvolucao){
         this.numIdEvolucao = numIdEvolucao;
     }
-    
-    public void setEvolucao(boolean evolucao){
+    /**
+     * classe com objetivo de verificar se o pokemon que será setado, é uma
+     * evolução ou não.
+     * @param evolucao 
+     */
+    public void setEvolucao(int evolucao){
         this.evolucao = evolucao;
     }
-    
-    public boolean getEvolucao(){
+    /**
+     * 
+     * @return se o pokemon é uma evolução ou não.
+     */
+    public int getEvolucao(){
         return this.evolucao;
+    }
+    
+    public String getNomePokemon(){
+        return this.nomePokemon;
+    }
+    
+    public String getSexoPokemon(){
+        return this.sexoPokemon;
+    }
+    
+    public String getHabilidade(){
+        return this.habilidade;
+    }
+    
+    public String getFraqueza(){
+        return this.fraqueza;
+    }
+    
+    public String getDescricao(){
+        return this.descricao;
+    }
+    
+    public int getAltura(){
+        return this.altura;
+    }
+    
+    public int getPeso(){
+        return this.peso;
+    }
+    
+    public int getIdNumPokemon(){
+        return Pokemon.numId;
+    }
+    
+    public int getNumIdEvolucao(){
+        return this.getIdNumPokemon();
     }
     /**
      * exclui pokemon
      */
-    @Override
+    /*@Override
     public void excluiPokemon(int numId) {
         //...
-    }
+    }*/
 }
