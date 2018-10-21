29462fbd43a5e3326fa43ac13ad2497bb4b8ee9a
pedro <pedro@172.18.133.64>
2018-10-21 21:55:43 -0200

    Adicionando Ginasio no arrayList de ginasio, get's objeto ginasio (todos os objetos) e de um objeto específico. Além de criar a opçao de excluir ginásio.

--- Pokemon-Trabalho/src/pokemon/trabalho/Pokedex.java
+++ Pokemon-Trabalho/src/pokemon/trabalho/Pokedex.java
@@ -16,22 +16,42 @@ public class Pokedex extends Jogador {
     private static  int numGinasio = 0;
     
     //Pokemon pok = new Pokemon();
+    
+    //pesquisar List em vez de arrayList
     private static ArrayList <Pokemon> arrayPokemon = new ArrayList();
+    private static ArrayList <Ginasio> arrayGinasio = new ArrayList();
 
-
+    /**
+     * Adicionando o objeto ginasio no ArrayList e somando a qtd de ginasio
+     * @param gin 
+     */
+    public void setObjetoGinasio(Ginasio gin){
+        Pokedex.numGinasio++;
+        Pokedex.arrayGinasio.add(gin);
+    }
     /**
      * criando o metodo setPokemon, que usará os metodos set's da classe filha
      * 'Pokemon'
      * @param pokk
      */
-    
     public void setObjetoPokemon(Pokemon pokk){
         Pokedex.numPokemons++;
         Pokedex.arrayPokemon.add(pokk);
         
     }
-    
-    public void getObjetoPokemon(){
+    /**
+     * 
+     * @param ind indece do objeto no array Pokemon
+     * @return o objeto referente ao ind passado por parametro
+     */
+    public Pokemon getObjetoPokemon(int ind){
+     return Pokedex.arrayPokemon.get(ind);
+    } 
+
+    /**
+     *Imprime todas as informações do array Pokemon
+     */
+    public void getObjetoPokemonLista(){
         String pokemon;
         if(Pokedex.arrayPokemon.isEmpty()){
             System.out.println("Não há nenhum pokemon registrado na pokedex!");
@@ -57,79 +77,47 @@ public class Pokedex extends Jogador {
         }
         
     }
-    //public void setPokemon(){
-      //  setNumPokemon();
-        
-        /*String auxString;
-        int auxNum;
-        boolean auxBool;
-        //Set nome pokemon
-        System.out.println("Digite o nome do pokemon");
-        Scanner nomePok = new Scanner(System.in);
-        auxString = nomePok.next();
-        arrayPokemon.get(numPokemons).setNomePokemon(auxString);
-        //set sexo pokemon
-        System.out.println("Digite o sexo do pokemon");
-        Scanner sexoPok = new Scanner(System.in);
-        auxString = sexoPok.next();
-        arrayPokemon.get(numPokemons).setSexoPokemon(auxString);
-        //set habilidade pokemon
-        System.out.println("Digite a habilidade do pokemon");
-        Scanner habilidadePok = new Scanner(System.in);
-        auxString = habilidadePok.next();
-        arrayPokemon.get(numPokemons).setHabilidade(auxString);
-        //set fraqueza pokemon
-        System.out.println("Digite a fraqueza do pokemon");
-        Scanner fraquezaPok = new Scanner(System.in);
-        auxString = fraquezaPok.next();
-        arrayPokemon.get(numPokemons).setFraqueza(auxString);
-        //set descrição pokemon
-        System.out.println("Digite descricao do pokemon");
-        Scanner descricaoPok = new Scanner(System.in);
-        auxString = descricaoPok.next();
-        arrayPokemon.get(numPokemons).setDescricao(auxString);
-        //set altura pokemon
-        System.out.println("Digite a altura do pokemon");
-        Scanner alturaPok = new Scanner(System.in);
-        auxNum = alturaPok.nextInt();
-        arrayPokemon.get(numPokemons).setAltura(auxNum);
-        //set peso pokemon
-        System.out.println("Digite o peso do pokemon");
-        Scanner pesoPok = new Scanner(System.in);
-        auxNum = pesoPok.nextInt();
-        arrayPokemon.get(numPokemons).setPeso(auxNum);
-        //set idNum pokemon
-        arrayPokemon.get(numPokemons).setNumId();
-        //set evolucao pokemon
-        System.out.println("O pokemon é uma evolução? Sim(1) ou Não(0)");
-        Scanner evolucaoPok = new Scanner(System.in);
-        auxBool = evolucaoPok.nextBoolean();
-        arrayPokemon.get(numPokemons).setEvolucao(auxBool);
-        //set numIdEvolução pokemon
-        if(arrayPokemon.get(numPokemons).getEvolucao()){
-            System.out.println("Digite id(identificador) do pokemon 'pai'");
-            Scanner idPok = new Scanner(System.in);
-            auxNum = idPok.nextInt();
-            arrayPokemon.get(numPokemons).setNumIdEvolucao(auxNum);
-        }*/
-        
-        
-        
-    //}
-    /**
-     * adicionando o numero de pokemons na pokedex.
+     /**
+     *Imprime todas as informações do array Pokemon
      */
-    /*public void setNumPokemon(){
+    public void getObjetoGinasioLista(){
+        String ginasio;
+        if(Pokedex.arrayGinasio.isEmpty()){
+            System.out.println("Não há nenhum ginasio registrado na pokedex!");
+        }else{
+            
+            for(int aux = 0 ; aux< Pokedex.arrayGinasio.size(); aux++){
+                
+                //Pokemon auxPokk = this.arrayPokemon.get(aux);
+                //System.out.println("Tamanho do arrayList: "+Pokedex.arrayPokemon.size()+"Posição: "+aux);
+                ginasio = " | Nome do Ginasio: "+Pokedex.arrayGinasio.get(aux).getNomeGinasio()+"\n | "
+                        + "Nome do mestre de ginasio: "+Pokedex.arrayGinasio.get(aux).getMestreGinasio();
+                System.out.println(ginasio);
+                ArrayList <Pokemon> auxArray = new ArrayList();
+                auxArray = Pokedex.arrayGinasio.get(aux).getPokemons();
+                for(int aux1 = 0 ; aux1 < Pokedex.arrayGinasio.get(aux).getPokemons().size() ;aux1++){
+                    System.out.println(" | Nome do Pokemon: "+auxArray.get(aux1).getNomePokemon());
+                }
+                System.out.println("\n ----------");
+            }
         
-        Pokedex.numPokemons++;
-    }*/
+        }
+        
+    }
     
+    /** 
+     * @param ind indece do objeto no array Ginasio
+     * @return o objeto referente ao ind passado por parametro
+     */
+    public Ginasio getObjetoGinasio(int ind){
+     return Pokedex.arrayGinasio.get(ind);
+    } 
     /**
      * adicionando o numero de ginasio que está na pokedex.
      */
-    public void setNumGinasio(){
+    /*public void setNumGinasio(){
         Pokedex.numGinasio++;
-    }
+    }*/
     /**
      * 
      * @return o numero de pokemons que possui na pokedex.
@@ -148,6 +136,7 @@ public class Pokedex extends Jogador {
     /**
      * atualiza o numero de pokemon
      * @param numId
+     * excluindo o pokemon da lista de acordo com o id passado.
      */
     public void excluiPokemon(int numId){
         try{
@@ -160,7 +149,21 @@ public class Pokedex extends Jogador {
                 }
                 }
     }
-    public void excluiGinasio(){
-        //...
+    /**
+     * exclui o ginásio de acordo com o id passado.
+     * @param ind 
+     */
+  
+    public void excluiGinasio(int ind){
+        if(ind+1 > arrayGinasio.size() || ind < 0){
+                    System.out.println("Ginasio não encontrado!");
+        }else{
+            arrayGinasio.remove(ind);
+            Pokedex.numGinasio--;
+            System.out.println("Ginasio deletado com sucesso!");
+        }
+        
     }
+    
+
 }
